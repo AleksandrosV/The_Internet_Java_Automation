e940f4839dd6fe4e9b793ca929a09b1ec896242a
Aleksandros Vuros <aleksandrosvouros@gmail.com>
2024-02-15 12:16:03 +0200

    Edito Page tests changes

--- AutomationWebUI/src/test/java/com/herokuapp/theinternet/editortests/EditorTests.java
+++ AutomationWebUI/src/test/java/com/herokuapp/theinternet/editortests/EditorTests.java
@@ -5,27 +5,14 @@ import org.testng.annotations.Test;
 
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.EditorPage;
-import com.herokuapp.theinternet.pages.WelcomePage;
 
 public class EditorTests extends TestUtilities {
 	@Test
 	public void defaultEditorValueTest() {
 		log.info("Starting defaultEditorValueTest");
-
-		// open main page
-		WelcomePage welcomePage = new WelcomePage(driver, log);
-		welcomePage.openPage();
-
-		// Scroll to the bottom
 		welcomePage.scrollToBottom();
-
-		// Click on WYSIWYG Editor link
 		EditorPage editorPage = welcomePage.clickWYSIWYGEditorLink();
-
-		// Get default editor text
 		String editorText = editorPage.getEditorText();
-
-		// Verification that new page contains expected text in source
 		Assert.assertTrue(editorText.equals("Your content goes here."),
 				"Editor default text is not expected. It is: " + editorText);
 	}
