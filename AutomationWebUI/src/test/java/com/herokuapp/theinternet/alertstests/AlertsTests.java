2179b97662dc678f685e7bac0539b1b187695c55
Aleksandros Vuros <aleksandrosvouros@gmail.com>
2024-02-20 19:25:05 +0200

    JS alerst tests changes

--- AutomationWebUI/src/test/java/com/herokuapp/theinternet/alertstests/AlertsTests.java
+++ AutomationWebUI/src/test/java/com/herokuapp/theinternet/alertstests/AlertsTests.java
@@ -5,73 +5,33 @@ import org.testng.asserts.SoftAssert;
 
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.JavaScriptAlertsPage;
-import com.herokuapp.theinternet.pages.WelcomePage;
 
 public class AlertsTests extends TestUtilities {
 	@Test
 	public void jsAlertTest() {
 		SoftAssert softAssert = new SoftAssert();
-
-		// open main page
-		WelcomePage welcomePage = new WelcomePage(driver, log);
-		welcomePage.openPage();
-
-		// Click on JavaScript Alerts link
 		JavaScriptAlertsPage alertsPage = welcomePage.clickJavaScriptAlertsLink();
-
-		// Click JS Alert button
 		alertsPage.openJSAlert();
-		sleep(1000);
-
-		// Get alert text
 		String alertMessage = alertsPage.getAlertText();
-
-		// Click OK button
 		alertsPage.acceptAlert();
-
-		// Get Results text
 		String result = alertsPage.getResultText();
-		sleep(1000);
-		// Verifications
-		// 1 - Alert text is expected
 		softAssert.assertTrue(alertMessage.equals("I am a JS Alert"),
 				"Alert message is not expected. \nShould be 'I am a JS Alert', but it is '" + alertMessage + "'");
-
-		// 2 - Result text is expected
-		softAssert.assertTrue(result.equals("You successfuly clicked an alert"),
-				"result is not expected. \nShould be 'You successfuly clicked an alert', but it is '" + result + "'");
+		softAssert.assertTrue(result.equals("You successfully clicked an alert"),
+				"result is not expected. \nShould be 'You successfully clicked an alert', but it is '" + result + "'");
 		softAssert.assertAll();
 	}
 
 	@Test
 	public void jsDismissTest() {
 		SoftAssert softAssert = new SoftAssert();
-
-		// open main page
-		WelcomePage welcomePage = new WelcomePage(driver, log);
-		welcomePage.openPage();
-
-		// Click on JavaScript Alerts link
 		JavaScriptAlertsPage alertsPage = welcomePage.clickJavaScriptAlertsLink();
-
-		// Click JS Confirm button
 		alertsPage.openJSConfirm();
-		sleep(1000);
-		// Get alert text
 		String alertMessage = alertsPage.getAlertText();
-
-		// Click Cancel button
 		alertsPage.dismissAlert();
-
-		// Get Results text
 		String result = alertsPage.getResultText();
-		sleep(1000);
-		// Verifications
-		// 1 - Alert text is expected
 		softAssert.assertTrue(alertMessage.equals("I am a JS Confirm"),
 				"Alert message is not expected. \nShould be 'I am a JS Confirm', but it is '" + alertMessage + "'");
-
-		// 2 - Result text is expected
 		softAssert.assertTrue(result.equals("You clicked: Cancel"),
 				"result is not expected. \nShould be 'You clicked: Cancel', but it is '" + result + "'");
 		softAssert.assertAll();
@@ -80,32 +40,13 @@ public class AlertsTests extends TestUtilities {
 	@Test
 	public void jsPromptTest() {
 		SoftAssert softAssert = new SoftAssert();
-
-		// open main page
-		WelcomePage welcomePage = new WelcomePage(driver, log);
-		welcomePage.openPage();
-
-		// Click on JavaScript Alerts link
 		JavaScriptAlertsPage alertsPage = welcomePage.clickJavaScriptAlertsLink();
-
-		// Click JS Prompt button
 		alertsPage.openJSPrompt();
-		sleep(1000);
-		// Get alert text
-		String alertMessage = alertsPage.getAlertText() + "[FAIL]";
-
-		// Type text into alert
+		String alertMessage = alertsPage.getAlertText();
 		alertsPage.typeTextIntoAlert("Hello Alert, it's Dmitry here");
-		sleep(1000);
-		// Get Results text
-		String result = alertsPage.getResultText() + "[FAIL]";
-		sleep(1000);
-		// Verifications
-		// 1 - Alert text is expected
+		String result = alertsPage.getResultText();
 		softAssert.assertTrue(alertMessage.equals("I am a JS prompt"),
 				"Alert message is not expected. \nShould be 'I am a JS prompt', but it is '" + alertMessage + "'");
-
-		// 2 - Result text is expected
 		softAssert.assertTrue(result.equals("You entered: Hello Alert, it's Dmitry here"),
 				"result is not expected. \nShould be 'You entered: Hello Alert, its Dmitry here', but it is '" + result
 						+ "'");
