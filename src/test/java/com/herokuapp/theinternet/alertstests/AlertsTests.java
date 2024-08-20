ec2011787170d3bd492a499f08456255dc5c1144
Aleksandros Vuros <aleksandrosvouros@gmail.com>
2024-08-20 18:39:08 +0300

    test

--- src/test/java/com/herokuapp/theinternet/alertstests/AlertsTests.java
+++ src/test/java/com/herokuapp/theinternet/alertstests/AlertsTests.java
@@ -17,7 +17,7 @@ public class AlertsTests extends TestUtilities {
 	}
 
 	@Test
-	public void jsAlertTestw() {
+	public void jsAlertTest() {
 		SoftAssert softAssert = new SoftAssert();
 		alertsPage.openJSAlert();
 		String alertMessage = alertsPage.getAlertText();
